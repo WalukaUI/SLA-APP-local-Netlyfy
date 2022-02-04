e9bef8fa2f1d07d499962f55e3a09c273fd44713
WalukaUI <cwaluka@yahoo.com>
2022-02-03 18:07:04 -0800

    feb 4th flag added

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -3,8 +3,17 @@ import { Carousel } from "react-bootstrap";
 import "./Carousel.css";
 
 function Carosel() {
-  return <div className="carousel">
+  return (
+    <div className="carousel">
       <Carousel fade>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="../img/flag5.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption className="description"></Carousel.Caption>
+        </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block w-100"
@@ -12,7 +21,7 @@ function Carosel() {
             alt="Second slide"
           />
           <Carousel.Caption className="description">
-          <h5 className="carasolevent">Coming Soon...</h5>
+            <h5 className="carasolevent">Coming Soon...</h5>
             <h3> Collecting goods for Afghan refugees</h3>
             <a href="!#">more information will be available soon...</a>
           </Carousel.Caption>
@@ -21,7 +30,7 @@ function Carosel() {
           <img className="d-block w-100" src="../img/3.jpg" alt="Third slide" />
 
           <Carousel.Caption className="description">
-          <h5 className="carasolevent">Ongoing event...</h5>
+            <h5 className="carasolevent">Ongoing event...</h5>
             <h3> O2 for Sri Lanka initiative</h3>
             <a
               href=" https://www.gofundme.com/f/Sri-Lanka-Covid-Help-ICU-Oxygen?fbclid=IwAR0h9OI1eMIscrYNOMU77obfjoaeUKSqYpGg0cU6MPYXYsTVKShIWLpWuEc"
@@ -34,7 +43,8 @@ function Carosel() {
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
-      </div>
+    </div>
+  );
 }
 
 export default Carosel;
